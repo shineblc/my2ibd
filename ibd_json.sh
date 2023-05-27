f55e715f0a17ff850a259df210c895b0d50fb878
不吃萝卜 <57123150+shineblc@users.noreply.github.com>
2023-05-27 13:41:04 +0800

    Update ibd_json.sh

--- ibd_json.sh
+++ ibd_json.sh
@@ -16,4 +16,6 @@ for file in $basedir/*.ibd
 do
 data=${file##*/}
 ibd2sdi $file >$dir_json/$data.json
+#因为全局索引是会生成fts_0000000000开头的文件，解析时候并不需要，所以rm即可,注意此处也可以自行rm
+#rm -rf $dir_json/fts_00000000*
 done
